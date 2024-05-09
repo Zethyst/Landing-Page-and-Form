5441b07961fed732bbee0be898f34f6003fb044a
Zethyst <zethyst@protonmail.com>
2024-05-09 16:42:01 +0530

    Updated Readme

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 <>
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/Landing-Page-and-Form" element={<Home/>}/>
         <Route exact path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
